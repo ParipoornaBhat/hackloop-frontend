267bc68cdb5b359f98cc61afb3ea4c65adb3d34b
ParipoornaBhat <157298696+ParipoornaBhat@users.noreply.github.com>
2024-11-23 13:12:00 +0530

    s2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests starting with "/api" to the backend server (localhost:5000)
       "/api": {
-        target: "https://sarvam-mern.onrender.com", // Backend server URL
+        target: "https://hackloop-backend.onrender.com", // Backend server URL
         changeOrigin: true, // Ensure proper origin handling
         rewrite: (path) => path.replace("/^/api/", ""), // Optionally remove /api prefix
       },
